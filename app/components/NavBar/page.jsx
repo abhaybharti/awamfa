c5609a661cb9d7b8f24397db08ee5c369a718b71
Abhay Bharti <abhaybharti@users.noreply.github.com>
2024-07-09 21:00:35 +0530

    added Home tab

--- app/components/NavBar/page.jsx
+++ app/components/NavBar/page.jsx
@@ -5,8 +5,10 @@ const NavBar = () => {
   return (
     <div className="flex h-16 align-middle bg-[#232F3F] justify-center text-white">
       <ul className="flex flex-row space-x-3 pr-2 pl-2 mt-4 justify-center align-middle">
+      
+      <li><Link href="/">Home</Link></li>
         <li className="">
-          {" "}
+          
           <Link href="/components/RequestForQuestions">
             Request For Security Questions
           </Link>
