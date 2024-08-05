222103dc846741e45b9d87d3b269d9f9ea863963
Abhay Bharti <abhaybharti@users.noreply.github.com>
2024-08-05 19:57:54 +0530

    added bar chart

--- app/components/BarChart/page.tsx
+++ app/components/BarChart/page.tsx
@@ -1,7 +1,97 @@
+"use client";
+import { Bar } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarController,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
 
+ChartJS.register(
+  Title,
+  Tooltip,
+  Legend,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  BarController
+);
 
 const BarChart = () => {
-  return <div>Bar Chart</div>;
+  const data = {
+    labels: ["Label 1", "Label 2", "Label 3", "Label 4", "Label 5"],
+    datasets: [
+      {
+        label: "Data Series 1",
+        backgroundColor: "rgba(0, 191, 255, 0.6)",
+        borderColor: "rgba(75, 192, 192, 1)",
+        borderWidth: 1,
+        data: [12, 19, 3, 5, 2],
+      },
+      {
+        label: "Data Series 2",
+        backgroundColor: "rgba(255, 99, 71, 0.8)",
+        borderColor: "rgba(75, 192, 192, 1)",
+        borderWidth: 1,
+        data: [12, 19, 3, 5, 2],
+      },
+    ],
+  };
+
+  const options = {
+    elements: {
+      bar: {
+        borderRadius: 10,
+        borderWidth: 0.7,
+      },
+    },
+    scales: {
+      y: {
+        beginAtZero: true,
+        border: { dash: [6, 6], display: true },
+        grid: {
+          display: true, // Display grid lines for the y-axis
+        },
+        ticks: {
+          padding: 15,
+        },
+      },
+      x: {
+        beginAtZero: true,
+        border: { display: true },
+        grid: {
+          display: false, // No display of grid lines for the x-axis
+        },
+        ticks: {
+          padding: 7,
+        },
+      },
+    },
+    layout: {
+      padding: {
+        left: 5,
+        right: 5,
+        top: 10,
+        bottom: 5,
+      },
+      margin: {
+        left: 5,
+        right: 5,
+        top: 5,
+        bottom: 5,
+      },
+    },
+  };
+
+  return (
+    <div>
+      <Bar data={data} options={options} />
+    </div>
+  );
 };
 
 export default BarChart;
